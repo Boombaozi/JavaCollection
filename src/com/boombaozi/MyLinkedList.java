d982fdc14ee51b2dd05b882958a63d14eaf5a000
huiyuhang123@qq.com <huiyuhang123@qq.com>
2018-04-17 16:11:09 +0800

    init

---
+++ src/com/boombaozi/MyLinkedList.java
@@ -0,0 +1,10 @@
+package com.boombaozi;
+
+/**
+ * @program: myCollection
+ * @description:
+ * @author: boombaozi.com
+ * @create: 2018-04
+ **/
+public class MyLinkedList<E> {
+}
---
+++ src/com/boombaozi/MyList.java
@@ -0,0 +1,10 @@
+package com.boombaozi;
+
+/**
+ * @program: myCollection
+ * @description: MyList interface
+ * @author: boombaozi.com
+ * @create: 2018-04-17 16
+ **/
+public interface MyList<E>  {
+}
